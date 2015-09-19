9bec4ad957740f17c6a8e9bb93dd0fafba1abe01
david <davidcueva8@gmail.com>
2015-09-18 19:47:02 -0700

    homework

--- rubyfile.rb
+++ rubyfile.rb
@@ -1,1 +1,11 @@
-puts "hello world"
+print "What's your first name?"
+first_name = gets.chomp
+print "Where we grew up?"
+grew_up = gets.chomp
+print "what is your current ocupation?"
+occupation = gets.chomp
+print "hidden talent?"
+hidden_talent = gets.chomp
+
+puts "Your name is #{first_name} where are you from #{grew_up}
+what is your occupation #{occupation},any hiddent talent #{hidden_talent}!”
