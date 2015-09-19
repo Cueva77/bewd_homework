bab02dd3667f63a0aa9fbf1949f41e7d71aa7598
david <davidcueva8@gmail.com>
2015-09-19 10:54:31 -0700

    animal sound

--- rubyfile.rb
+++ rubyfile.rb
@@ -1,14 +1,12 @@
-puts "dog"
+puts "enter the name of an animal that makes a sound"
 name = gets.chomp
+puts "you entered " + name.downcase
+puts "what sound does a " + name.downcase + " make? "
+sound = gets.chomp
+puts "you entered " + sound.downcase
+puts "The " + name.downcase + " goes " + sound.downcase.strip + " " + sound.downcase.strip + " " + sound.downcase.strip
 
-puts "dog" + name.downcase
+puts "The " + name.downcase + " goes " + sound.downcase.strip + " how many times?"
+num = gets.chomp.to_i
 
-puts "what sound does a" + name.downcase + "make"
-sound = get.chomp
-
-puts "woof" + sound.downcase
-
-puts "the" + name.downcase + "goes" + sound.downcase.strip + " " +
-sound.downcase.strip + " " + sound.downcase.strip
-
-puts "the" + name.downcase + "goes" + (sound.downcase + "," + " ") * (num - 1) + sound.downcase + "."
+puts "The " + name.downcase + " goes " + (sound.downcase + "," + " ") * (num - 1) + sound.downcase + "."
